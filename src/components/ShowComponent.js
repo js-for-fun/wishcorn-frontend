0614054cc575fce35fa95d9ba74ee69c71fbb539
Dmitry Vlasenko <dvlasenko96@gmail.com>
2016-05-09 22:33:12 +0300

    Connect client to server (stable)

--- src/components/ShowComponent.js
+++ src/components/ShowComponent.js
@@ -4,22 +4,61 @@ import React from 'react';
 
 import {Button} from 'react-bootstrap';
 
+
 require('styles/Show.scss');
 
 class ShowComponent extends React.Component {
+    constructor(props, context) {
+        super(props, context);
+        this.state = {
+            corn: {}
+        };
+    }
+
     yesHandler() {
-        console.log('Yes');
+        this.increment();
+        this.getRandom();
     }
     noHandler() {
-        console.log('No');
+        this.getRandom();
+    }
+
+    getRandom() {
+        fetch('/corns/random', {credentials: 'same-origin'}).then((res) => {
+            return res.json();
+        }).then((json) => {
+            json.img = json.img.replace('{index}', 5);
+
+            this.setState({corn: json});
+        });
+    }
+
+    increment() {
+        fetch('/corns/increment', {
+            method: 'POST',
+            body: JSON.stringify({ category: this.state.corn.category }),
+            headers: {
+                'Accept': 'application/json',
+                'content-type': 'application/json'
+            },
+            credentials: 'same-origin'
+        });
+    }
+
+    componentWillMount() {
+        this.getRandom();
     }
     render() {
         return (
             <div className="show-component">
                 <div className="container">
-                    <img src="https://i.ytimg.com/vi/FJEKF3iuvP8/hqdefault.jpg"/>
-                    <Button className="col-md-6" onClick={this.yesHandler}>YES</Button>
-                    <Button className="col-md-6" onClick={this.noHandler}>NO</Button>
+                    <h1>{this.state.corn.name}</h1>
+                    <img src={this.state.corn.img}/>
+                    <div dangerouslySetInnerHTML={{
+                        __html: this.state.corn.iframe
+                    }}></div>
+                    <Button className="col-md-6" onClick={this.yesHandler.bind(this)}>YES</Button>
+                    <Button className="col-md-6" onClick={this.noHandler.bind(this)}>NO</Button>
                 </div>
             </div>
         );
